9965566555f133ca45969572207a1c6661c2eadf
Youssef-Ayed <ayedy40@gmail.com>
2025-07-24 15:12:18 +0100

    test_main.h

---
+++ test/test_main.h
@@ -0,0 +1,11 @@
+#ifndef TEST_MAIN_H
+#define TEST_MAIN_H
+
+// Include necessary headers
+#include "unity.h"
+
+// Declare test functions
+void test_example_case(void);
+void test_another_case(void);
+
+#endif // TEST_MAIN_H
